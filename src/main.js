53a1be7dad5a7aaabb365d2cf562ae1053ea0018
pan <1573400829@qq.com>
2022-01-25 21:17:12 +0800

    项目初始化-调整初始项目目录结构

--- src/main.js
+++ src/main.js
@@ -1,8 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import style from './styles/index.less'
 
 Vue.config.productionTip = false
+// Vue.use(style)
 
 new Vue({
   router,
